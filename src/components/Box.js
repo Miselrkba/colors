27d270d8d4947fe38035761eceef147a6a28eae6
iii <miselrkba@gmail.com>
2020-09-01 17:57:34 +0200

    refactored - fully working now

--- src/components/Box.js
+++ src/components/Box.js
@@ -1,14 +1,63 @@
-import React from "react";
-import "./boxes.css";
+import React, { Component } from "react";
+import { choice } from "./helper";
 
-export const Box = (props) => {
-  const handleClick = (e) => {
-    props.onClick(e.target.value);
+class Box extends Component {
+  static defaultProps = {
+    allColors: [
+      "#ffcc66",
+      "#f9c267",
+      "#f2b869",
+      "#ecad6a",
+      "#e6a36b",
+      "#df996c",
+      "#d98f6e",
+      "#d3856f",
+      "#cc7a70",
+      "#c67072",
+      "#c06673",
+      "#b95c74",
+      "#b35276",
+      "#ac4777",
+      "#a63d78",
+      "#a0337a",
+      "#99297b",
+      "#931f7c",
+      "#8d147d",
+      "#860a7f",
+      "#800080",
+    ],
+  };
+  state = {
+    color: choice(this.props.allColors),
   };
 
-  return (
-    <div className='box' onClick={handleClick} style={{ background: `${props.color}` }}>
-      box
-    </div>
-  );
-};
+  pickColor = () => {
+    let newColor;
+    do {
+      newColor = choice(this.props.allColors);
+      //below you set the condition for stoping
+      //so if the color is already in state it stops
+      //this way you can never have two same colors showing
+    } while (newColor === this.state.color);
+
+    this.setState({
+      color: newColor,
+    });
+  };
+
+  handleClick = () => {
+    this.pickColor();
+  };
+
+  render() {
+    return (
+      <div
+        className="Box"
+        style={{ backgroundColor: this.state.color }}
+        onClick={this.handleClick}
+      ></div>
+    );
+  }
+}
+
+export default Box;
--- src/components/ColorContainer.js
+++ src/components/ColorContainer.js
@@ -1,63 +1,18 @@
 import React, { Component } from "react";
-import { Box } from "./Box";
+import Box from "./Box";
 import "./boxes.css";
-import { choice } from "./helper";
-import uuid from "react-uuid";
+// import { choice } from "./helper";
+// import uuid from "react-uuid";
 
 export default class ColorContainer extends Component {
   static defaultProps = {
-    colors: [
-      "#ffcc66",
-      "#f9c267",
-      "#f2b869",
-      "#ecad6a",
-      "#e6a36b",
-      "#df996c",
-      "#d98f6e",
-      "#d3856f",
-      "#cc7a70",
-      "#c67072",
-      "#c06673",
-      "#b95c74",
-      "#b35276",
-      "#ac4777",
-      "#a63d78",
-      "#a0337a",
-      "#99297b",
-      "#931f7c",
-      "#8d147d",
-      "#860a7f",
-      "#800080",
-    ],
-  };
-
-  state = {
-    count: 0,
-  };
-
-  handleClick = () => {
-    const rand = Math.floor(Math.random() * this.props.colors.length);
-    this.setState((curState) => ({
-      count: curState.count + rand,
-    }));
+    numBoxes: 18,
   };
 
   render() {
-    const getColor = choice(this.props.colors);
-    console.log(getColor);
-    let boxes = [];
-    while (boxes.length < 18) {
-      boxes.push(
-        <Box key={uuid()} onClick={this.handleClick} color={choice(this.props.colors)} />
-      );
-    }
-    
-    return (
-      <div>
-        {this.state.count}
-        <div className="container">{boxes}</div>
-        <button onClick={this.handleClick}>onClick</button>
-      </div>
-    );
+    const boxes = Array.from({ length: this.props.numBoxes }).map(() => (
+      <Box />
+    ));
+    return <div className="BoxContainer">{boxes}</div>;
   }
 }
